e1910f579604c1674c71ab24380b88e0dc213257
aashiqms <aashiqms@outlook.com>
2020-05-08 00:54:43 -0700

    my first commit

---
+++ ask/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from ask.users.models import User
+from ask.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
---
+++ post_question/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PostQuestionConfig(AppConfig):
+    name = 'post_question'
